879fe6341d1d3ea924962772a117961bbaf71579
Sixiang Miao <sixiangm@usc.edu>
2023-03-06 01:09:39 -0800

    34

--- Whac-a-Mole/Whac.js
+++ Whac-a-Mole/Whac.js
@@ -30,14 +30,18 @@ const Controller = ((view, model) => {
     const {domSelector} = view
     const map = {}
     let remain = 3;
+    const snakemap = {}
+    let gameover = false
+    let snakeallow = 0
     // let time = 30;
     domSelector.time.innerHTML = "0"
     for (let i = 1; i <= 12; i ++){
         map[i] = 0
+        snakemap[i] = 0
     }
     const generateRandom = (remain) =>{
         let ran = Math.floor(Math.random() * 12) + 1;
-        while(map[ran]){
+        while(map[ran] > 0 && snakemap[ran] > 0){
             ran = Math.floor(Math.random() * 12) + 1;
         }
         return ran;
@@ -47,41 +51,91 @@ const Controller = ((view, model) => {
     const produce = () =>{
         const interval = setInterval(() => {
 
-            domSelector.time.innerHTML = (domSelector.time.innerHTML - 1).toString()
+            domSelector.time.innerHTML = (domSelector.time.innerHTML - 1).toString() /*Get the time left*/
             if (domSelector.time.innerHTML > 0){
                 if (remain > 0){
                     const ran = generateRandom(remain)
-                    // console.log(ran)
+                    console.log(map)
                     map[ran] = 1;
                     remain --;
                     domSelector[`cir${ran}`].style.backgroundImage = "url(./mole.jpeg)"
                     domSelector[`cir${ran}`].style.backgroundSize = "cover"
                 }
-            }else {
+                if(snakeallow >= 2){
+                    const snakeran = Math.floor(Math.random() * 12) + 1;
+                    domSelector[`cir${snakeran}`].style.backgroundImage = "url(./mine.jpeg)"
+                    domSelector[`cir${snakeran}`].style.backgroundSize = "cover"
+                    snakemap[snakeran] ++;
+                    if(map[snakeran] > 0){
+                        console.log("chuangle")
+                        map[snakeran] = 0;
+                        remain ++;
+                    }
+                    snakeallow = 0
+                }
+                else{
+                    snakeallow++
+                }
+            }else if(gameover === false){
                 alert(`You have got ${domSelector.score.innerHTML}`)
+                remain = 3
                 for (let i = 1; i <= 12; i++){
                     map[i] = 0
-                    domSelector[`cir${i}`].style.backgroundImage = "none"
+                    snakemap[i] = 0
                 }
                 clearInterval(interval)
+                // gameover = true
+            }else{
+                remain = 3
+                for (let i = 1; i <= 12; i++){
+                    map[i] = 0
+                    snakemap[i] = 0
+                }
+                alert(`Game over \nYou have got ${domSelector.score.innerHTML}`)
+                clearInterval(interval)
+                // clearInterval(snake())
             }
             // console.log(map)
 
-            for (let i in map){
+
+            for (let i = 1; i <= 12; i ++){
                 if(map[i] > 3) {
                     domSelector[`cir${i}`].style.backgroundImage = "none"
                     map[i] = 0;
                     remain ++;
                 }
                 if (map[i] > 0){
-                    map[i] ++
+                    map[i] ++;
+                }
+                if (snakemap[i] > 1){
+                    domSelector[`cir${i}`].style.backgroundImage = "none"
+                    snakemap[i] = 0
+                }
+                if(snakemap[i] > 0){
+                    snakemap[i] ++;
                 }
             }
+            console.log("shengyu" + remain)
+
         },1000)
         return interval
     }
     domSelector.container.addEventListener('click',(event)=>{
-        if(map[event.target.id.slice(3)]){
+        if(snakemap[[event.target.id.slice(3)]] > 0){
+            // clearInterval(snake())
+            clearInterval(produce())
+            // clearTimeout(startgame())
+            domSelector.time.innerHTML = "1";
+            gameover = true
+            for(let i = 1;  i <= 12; i ++){
+                domSelector[`cir${i}`].style.backgroundImage = "url(./mine.jpeg)"
+                domSelector[`cir${i}`].style.backgroundSize = "cover"
+                map[i] = 0
+                snakemap[i] = 0
+                remain = 3
+            }
+        }
+        if(map[event.target.id.slice(3)] > 0){
             domSelector[event.target.id].style.backgroundImage = "none"
             map[event.target.id.slice(3)] = 0;
             domSelector.score.innerHTML = (parseInt(domSelector.score.innerHTML) + 1).toString()
@@ -91,24 +145,45 @@ const Controller = ((view, model) => {
         // console.log(event.target.id.slice(3))
     })
     domSelector.start.addEventListener('click',() =>{
-        if (parseInt(domSelector.time.innerHTML) === 0){
+        if (parseInt(domSelector.time.innerHTML) <= 0){
             domSelector.time.innerHTML = "30";
             for (let i = 1; i <= 12; i ++){
                 map[i] = 0
+                domSelector[`cir${i}`].style.backgroundImage = "none"
+                snakemap[i] = 0
             }
+            gameover = false
             remain = 3
             domSelector.score.innerHTML = "0"
             let interval = produce();
-            setTimeout(()=>{
+            // let createSnake = snake();
+            const startgame = setTimeout(()=>{
                 clearInterval(interval)
+                // clearInterval(createSnake)
             },31000)
         }
         else{
             alert("Game in process")
         }
-
-
     })
+    // const snake = () =>{
+    //     const createSnake = setInterval(() => {
+    //         // domSelector.time.innerHTML = (domSelector.time.innerHTML - 1).toString()
+    //         if(domSelector.time.innerHTML > 0 ){
+    //             const snakeran = Math.floor(Math.random() * 12) + 1;
+    //             domSelector[`cir${snakeran}`].style.backgroundImage = "url(./mine.jpeg)"
+    //             domSelector[`cir${snakeran}`].style.backgroundSize = "cover"
+    //             snakemap[snakeran] ++;
+    //             if(map[snakeran] > 0){
+    //                 map[snakeran] = 0;
+    //                 remain ++;
+    //             }
+    //
+    //             console.log(snakemap)
+    //         }
+    //     },2000)
+    //     return createSnake
+    // }
     // setTimeout()
     return 1
 })(View,Model)
